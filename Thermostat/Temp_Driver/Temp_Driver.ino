42ca47999a6f2b44b8f53c3829ccc3fdb142ac5b
Josh <joshualilly91@gmail.com>
2014-03-09 15:58:47 -0400

    added led controllers

--- Thermostat/Temp_Driver/Temp_Driver.ino
+++ Thermostat/Temp_Driver/Temp_Driver.ino
@@ -2,14 +2,30 @@ int val = 0;
 
 void setup(){
   Serial.begin(9600);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 void loop(){
+  byte r_Data;
+  digitalWrite(8, HIGH);//Ac off
+  digitalWrite(9, HIGH);//heat off  
  int sense = analogRead(A1);
  val = map(sense, 0, 1023, 50, 105);
  delay(100); 
  Serial.println(val);
- 
+ if(Serial.available() > 0){
+ r_Data = Serial.read()-'0';
+ } 
+
+ if(r_Data == 1){
+   digitalWrite(9, LOW);//heat
+   delay(2000);
+ }
+ if(r_Data == 2){
+   digitalWrite(8, LOW); //AC
+   delay(2000);
+ }
 }
  
  
--- Thermostat/Thermostat_Control.py
+++ Thermostat/Thermostat_Control.py
@@ -1,21 +1,36 @@
 import serial
+#from tkinter import *
 from time import sleep
 
-
+ser = serial.Serial('COM3', 9600)
+sleep(2)
 class Temperature_Driver:
-    
     def __init__(self):
         return
         
-    def read_optimal_temp(self):    
-        ser = serial.Serial('COM3', 9600) # Establish the connection on a specific port
+    def read_optimal_temp(self): 
+        global ser
         while True:
             optimal_temp_in = ser.readline()
             newOTemp = optimal_temp_in.decode('utf-8')
             oTemp = newOTemp.translate(newOTemp.maketrans("\n\t\r", "   "))
             print(oTemp)
-            
+    #Not needed do thermostat controlling in arduino
+    def turn_on_Heat(self):
+        global ser
+        if ser.closed:
+            ser = serial.Serial('COM3', 9600)
+            sleep(2)
+            ser.write(b'1')   
+    #Not needed do thermostat controlling in arduino
+    def turn_on_ac(self):
+        global ser
+        ser.write(b'2')        
+
 def main():
     driver = Temperature_Driver()
     driver.read_optimal_temp()
+    driver.turn_on_Heat()
+    sleep(5000)
+    driver.turn_on_ac()
 if __name__=='__main__':main()                
