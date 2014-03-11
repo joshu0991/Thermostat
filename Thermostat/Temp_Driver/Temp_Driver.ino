03cfb707cb216dcb9cfecdeef018b3baebfb6e36
Josh <joshualilly91@gmail.com>
2014-03-10 23:45:41 -0400

    finished temperature sensing and input sections

--- Thermostat/Temp_Driver/Temp_Driver.ino
+++ Thermostat/Temp_Driver/Temp_Driver.ino
@@ -1,31 +1,47 @@
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
+OneWire oneWire(12);
+DallasTemperature sensors(&oneWire);
+
 int val = 0;
 
 void setup(){
+  sensors.begin();
   Serial.begin(9600);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
+  digitalWrite(8, HIGH);//Ac off
+  digitalWrite(9, HIGH);//heat off
 }
 
 void loop(){
-  byte r_Data;
-  digitalWrite(8, HIGH);//Ac off
-  digitalWrite(9, HIGH);//heat off  
- int sense = analogRead(A1);
+ byte r_Data;
+ sensors.requestTemperatures();
+ float current_temp1; 
+ current_temp1= sensors.getTempCByIndex(0);
+ float farenheit =((current_temp1 * 1.8) + 32.0);
+ delay(100);
+ //Serial.print("Temperature "); Serial.println(farenheit);
+ float sense = analogRead(A1);
  val = map(sense, 0, 1023, 50, 105);
  delay(100); 
  Serial.println(val);
- if(Serial.available() > 0){
- r_Data = Serial.read()-'0';
- } 
-
- if(r_Data == 1){
-   digitalWrite(9, LOW);//heat
-   delay(2000);
- }
- if(r_Data == 2){
-   digitalWrite(8, LOW); //AC
-   delay(2000);
- }
+ //String broadcast = (val + ", "+ farenheit + ", "+ current_temp1); 
+ //Serial.println(broadcast);
+ int controller = farenheit - val;
+  
+  if(controller >= 10){
+    digitalWrite(8, LOW);//heat
+    delay(5000);
+    digitalWrite(8, HIGH);
+  }
+  if(controller <= -10){
+  digitalWrite(9, LOW);//AC
+  delay(5000);
+  digitalWrite(9, HIGH);
+  }
+ 
 }
  
  
