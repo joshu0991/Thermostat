a58d2a12962e178c93bf1bd96652029b9dc1bc71
Josh <joshualilly91@gmail.com>
2014-03-12 00:27:35 -0400

    added python program finished most of arduino but needs debugged

--- Thermostat/Temp_Driver/Temp_Driver.ino
+++ Thermostat/Temp_Driver/Temp_Driver.ino
@@ -1,5 +1,6 @@
 #include <DallasTemperature.h>
 #include <OneWire.h>
+#include <stdlib.h>;
 
 OneWire oneWire(12);
 DallasTemperature sensors(&oneWire);
@@ -16,20 +17,26 @@ void setup(){
 }
 
 void loop(){
- byte r_Data;
  sensors.requestTemperatures();
  float current_temp1; 
+ float farenheit;
+ char tmpC[6];
+ char oTmp[6];
+ 
  current_temp1= sensors.getTempCByIndex(0);
- float farenheit =((current_temp1 * 1.8) + 32.0);
+ farenheit =((current_temp1 * 1.8) + 32.0);
+ dtostrf(current_temp1, 6, 2, tmpC);
+ 
  delay(100);
  //Serial.print("Temperature "); Serial.println(farenheit);
  float sense = analogRead(A1);
  val = map(sense, 0, 1023, 50, 105);
  delay(100); 
- Serial.println(val);
- //String broadcast = (val + ", "+ farenheit + ", "+ current_temp1); 
- //Serial.println(broadcast);
- int controller = farenheit - val;
+ dtostrf(val, 6, 2, oTmp);
+ 
+  Serial.println(tmpC);
+ 
+  int controller = farenheit - val;
   
   if(controller >= 10){
     digitalWrite(8, LOW);//heat
--- Thermostat/Thermostat_Control.py
+++ Thermostat/Thermostat_Control.py
@@ -1,36 +1,71 @@
 import serial
-#from tkinter import *
+from threading import Thread
+from tkinter import *
 from time import sleep
+import sys
 
-ser = serial.Serial('COM3', 9600)
-sleep(2)
+################################################################################################################
+
+try:
+    ser = serial.Serial('COM4', 9600)
+    sleep(2)
+except:
+    ser.close()
+    ser = serial.Serial('COM4', 9600)
+    sleep(2)
+    
+##################################################################################################################
+
 class Temperature_Driver:
-    def __init__(self):
-        return
+    
+##################################################################################################################    
+    
+    def __init__(self, master=None):
+        master.geometry('600x350+400+150')
+        frame = Frame(master)
+        frame.pack()
+        self.ud_temp = StringVar()
+        self.m_temp = StringVar()
+        Label(frame, textvariable=self.m_temp, font=('Helvetica', 40)).grid(row=2, column=0)
+        Label(frame, textvariable=self.ud_temp, font=('Helvetica', 40)).grid(row=4, column=0)
         
-    def read_optimal_temp(self): 
+##################################################################################################################    
+    
+    def read_temp(self): 
         global ser
+        counter = 0
         while True:
             optimal_temp_in = ser.readline()
             newOTemp = optimal_temp_in.decode('utf-8')
-            oTemp = newOTemp.translate(newOTemp.maketrans("\n\t\r", "   "))
-            print(oTemp)
-    #Not needed do thermostat controlling in arduino
-    def turn_on_Heat(self):
-        global ser
-        if ser.closed:
-            ser = serial.Serial('COM3', 9600)
-            sleep(2)
-            ser.write(b'1')   
-    #Not needed do thermostat controlling in arduino
-    def turn_on_ac(self):
-        global ser
-        ser.write(b'2')        
-
+            oTemp = newOTemp.translate(newOTemp.maketrans("\n\t\r", "   ")) 
+            
+            if counter == 0:
+                self.ud_temp.set('Set Temperature\n ' + str(oTemp) + ' F')
+                counter += 1 
+            else:
+                farenheit = self.cal_farenheit(oTemp)
+                temp = ('Measured Temperature\n ' + str(oTemp) + 'C' + '\n'  + str(farenheit) + '  F' )
+                self.m_temp.set(temp)
+                counter -= 1
+                
+##################################################################################################################                
+                
+    def cal_farenheit(self, valC):
+        valC = float(valC)
+        farenheit = ((valC * 1.8) + 32)
+        faren = str(farenheit)
+        faren = faren[0:5]
+        return faren
+    
+#######################################################################################################################    
+    
 def main():
-    driver = Temperature_Driver()
-    driver.read_optimal_temp()
-    driver.turn_on_Heat()
-    sleep(5000)
-    driver.turn_on_ac()
+    root = Tk()
+    root.wm_title('Thermostat')
+    driver = Temperature_Driver(root)
+    Thread(target=driver.read_temp).start()
+    root.mainloop()
+    sys.exit()
+    if ser.isOpen():
+        ser.close()
 if __name__=='__main__':main()                
