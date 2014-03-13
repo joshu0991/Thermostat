30e8db1106c9bd8000ece2546567bfdb1a356577
Josh <joshualilly91@gmail.com>
2014-03-12 20:15:29 -0400

    finishd GUI and fix bug with arduino code

--- Thermostat/Temp_Driver/Temp_Driver.ino
+++ Thermostat/Temp_Driver/Temp_Driver.ino
@@ -2,47 +2,50 @@
 #include <OneWire.h>
 #include <stdlib.h>;
 
+///manditory third party libs for temp sensing
 OneWire oneWire(12);
 DallasTemperature sensors(&oneWire);
 
 int val = 0;
 
 void setup(){
-  sensors.begin();
-  Serial.begin(9600);
-  pinMode(8, OUTPUT);
+  sensors.begin();//beigin sensing temp
+  Serial.begin(9600);//start baud rate
+  pinMode(8, OUTPUT);//uoutput pins
   pinMode(9, OUTPUT);
   digitalWrite(8, HIGH);//Ac off
   digitalWrite(9, HIGH);//heat off
 }
 
 void loop(){
- sensors.requestTemperatures();
+ sensors.requestTemperatures();//request temp from sensor
  float current_temp1; 
  float farenheit;
  
- current_temp1= sensors.getTempCByIndex(0);
- farenheit =((current_temp1 * 1.8) + 32.0);
+ current_temp1= sensors.getTempCByIndex(0);///get the temp from an index
+ farenheit =((current_temp1 * 1.8) + 32.0);//convert to farenheit
  
  delay(100);
- float sense = analogRead(A1);
- val = map(sense, 0, 1023, 50, 105);
+ float sense = analogRead(A1);//read potentiometer
+ val = map(sense, 0, 1023, 50, 105);//map values
  delay(100); 
  
- Serial.println(val);
+ Serial.println(val);//print values over serial port
  Serial.println(current_temp1);
  
-  int tempPlusFive = farenheit + 5;
+  int tempPlusFive = farenheit + 5;//calculate temps above and below by fiive degree offset
   int tempMinusFive = farenheit - 5;
-  
+  //if in +- 5 degrees turn heat and ac off
   if(val < tempPlusFive && val > tempMinusFive){
     digitalWrite(8, HIGH);
     digitalWrite(9, HIGH);
   }
+  //outside of range above turn on heat
   else if(val >= tempPlusFive){
     digitalWrite(8, HIGH);
     digitalWrite(9, LOW);//heat
   }
+  //below tange turn on ac
    else if(val <= tempMinusFive){
     digitalWrite(9, HIGH);
     digitalWrite(8, LOW);//AC
--- Thermostat/Thermostat_Control.py
+++ Thermostat/Thermostat_Control.py
@@ -4,6 +4,7 @@ from tkinter import *
 from time import sleep
 import sys
 
+
 ################################################################################################################
 
 try:
@@ -20,17 +21,22 @@ class Temperature_Driver:
     
 ##################################################################################################################    
     
+    
+    #Constructor for gui set up
     def __init__(self, master=None):
         master.geometry('600x350+400+150')
         frame = Frame(master)
         frame.pack()
         self.ud_temp = StringVar()
         self.m_temp = StringVar()
-        Label(frame, textvariable=self.m_temp, font=('Helvetica', 40)).grid(row=2, column=0)
-        Label(frame, textvariable=self.ud_temp, font=('Helvetica', 40)).grid(row=4, column=0)
+        
+        #Makes labels
+        Label(frame, textvariable=self.m_temp, relief=RIDGE, font=('red Helvetica', 40)).grid(row=2, column=0)
+        Label(frame, textvariable=self.ud_temp, relief=RIDGE, font=('blue Helvetica', 40)).grid(row=4, column=0)
         
 ##################################################################################################################    
     
+    #Reads a temp and writes it to the gui    
     def read_temp(self): 
         global ser
         counter = 0
@@ -39,17 +45,19 @@ class Temperature_Driver:
             newOTemp = optimal_temp_in.decode('utf-8')
             oTemp = newOTemp.translate(newOTemp.maketrans("\n\t\r", "   ")) 
             
+            #First value transmitted is the user defined optimal temp
             if counter == 0:
                 self.ud_temp.set('Set Temperature\n ' + str(oTemp) + ' F')
                 counter += 1 
             else:
-                farenheit = self.cal_farenheit(oTemp)
+                farenheit = self.cal_farenheit(oTemp) #second value transmitted is the measured room temperature
                 temp = ('Measured Temperature\n ' + str(oTemp) + 'C' + '\n'  + str(farenheit) + '  F' )
                 self.m_temp.set(temp)
                 counter -= 1
-                
+   
 ##################################################################################################################                
                 
+    #Convert to farenheit
     def cal_farenheit(self, valC):
         valC = float(valC)
         farenheit = ((valC * 1.8) + 32)
@@ -59,13 +67,15 @@ class Temperature_Driver:
     
 #######################################################################################################################    
     
+#Start main GUI
 def main():
     root = Tk()
     root.wm_title('Thermostat')
     driver = Temperature_Driver(root)
-    Thread(target=driver.read_temp).start()
+    Thread(target=driver.read_temp, name='driver').start()#Thread for updating temps
     root.mainloop()
     sys.exit()
     if ser.isOpen():
         ser.close()
+    
 if __name__=='__main__':main()                
